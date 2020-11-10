e8afa61857d013f7d6ad4ef4064fe13b74dcd4f7
Keunyoung-Jung <rmsiddl12@naver.com>
2020-11-10 14:09:14 +0900

    Add gcc install

--- install_steamer.sh
+++ install_steamer.sh
@@ -1,4 +1,5 @@
 sudo -S apt-get install git
+sudo -S apt-get install gcc g++
 sudo -S apt-get install cmake python-pil python3-pil libjpeg-dev build-essential
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer-experimental
